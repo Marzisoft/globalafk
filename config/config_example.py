f76b03f4bc2e6045f279e9af2b38f0f088cc70ae
myumyu <23904-myumyu@users.noreply.gitgud.io>
2021-10-24 21:26:10 +0100

    Added option to turn off unwanted watchers

--- config/config_example.py
+++ config/config_example.py
@@ -2,31 +2,32 @@ DOMAIN_NAME: str = "dummy.ib"
 MOD_USERNAME: str = "dummy_username"
 MOD_PASSWORD: str = "dummy_password"
 
-"""Custom session related, conservative values by default"""
-REQUEST_RETRIES: int = 6  # number of allowed retries
-RETRIES_BACKOFF_FACTOR: float = 3  # sleep factor between retries, defines how the backoff grows
-REQUEST_TIMEOUT: int = 15  # max wait time for a server response (in seconds)
-
 """Notification related"""
 USE_TERMUX_API: bool = False
 
 """Reports watcher related"""
+WATCH_REPORTS: bool = True
 FETCH_REPORTS_INTERVAL: int = 60 * 2  # interval between reports fetch (in seconds)
 
-"""Live posts watcher related"""
+"""Posts watcher related"""
+WATCH_LIVE_POSTS: bool = True
 LIVE_POSTS_RECONNECT_DELAY: int = 25  # delay between live posts socket reconnection (in seconds)
 
-"""Live posts evaluator related"""
-# text matches related
+# live posts text matches related
 TRIGGER_ON_BLACKLISTED: bool = True  # text that are blacklisted entries should trigger a notification?
 TRIGGER_OFFSET: int = 25  # number of characters showed before/after the trigger
 TRIGGER_WRAPPER: str = "*"  # string used to wrap the *trigger*
 BLACKLIST: tuple = (  # each entry must be a regex
     r"\bd+u+m+m+y*\b",
 )
-# urls matches related
+# live post url matches related
 TRIGGER_ON_NON_WHITELISTED_URLS: bool = False  # urls that are not whitelisted should trigger a notification?
 URL_WHITELIST: tuple = (  # each entry must be a regex
     r"(\w*:/+)*(\w*\.)*(dummy.com)(/+\w*)*",
 
 )
+
+"""Custom session related, conservative values by default"""
+REQUEST_RETRIES: int = 6  # number of allowed retries
+RETRIES_BACKOFF_FACTOR: float = 3  # sleep factor between retries, defines how the backoff grows
+REQUEST_TIMEOUT: int = 15  # max wait time for a server response (in seconds)
--- main.py
+++ main.py
@@ -8,21 +8,21 @@ from watchers import ReportsWatcher, LivePostsWatcher
 
 
 def send_notification(title, body):
-    logging.debug(f'Sending notification: {title} {body}')
+    logging.debug(f'Sending notification with title: {title} and body: {body}')
     subprocess.call(['termux-notification', '--title', title, '--content', body] if config.USE_TERMUX_API
                     else ['notify-send', title, body])
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s %(funcName)s] %(message)s (%(name)s)')
-
     session = ModSession()  # custom session that is authenticated and hopefully can be shared between watchers
-    reports_watcher = ReportsWatcher(session, send_notification)  # launches reports watcher
-    live_posts_watcher = LivePostsWatcher(session, send_notification,
-                                          Evaluator().evaluate)  # launches live posts watcher
-
-    reports_watcher.join()
-    live_posts_watcher.join()
+    watchers = list()
+    if config.WATCH_REPORTS:  # launches reports watcher
+        watchers.append(ReportsWatcher(session, send_notification))
+    if config.WATCH_LIVE_POSTS:  # launches live posts watcher
+        watchers.append(LivePostsWatcher(session, send_notification, Evaluator().evaluate))
+    for watcher in watchers:
+        watcher.join()
 
 
 if __name__ == '__main__':
